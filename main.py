1f40accff06a5dfc5d30a3fb2e05cc2a33a27807
gabdorada <gabdorada@gmail.com>
2026-05-05 22:06:49 -0300

    feat: add main script with ASCII logo

---
+++ main.py
@@ -0,0 +1,19 @@
+import random 
+
+def mostrar_logo():
+    logo = r"""
+                    /     \
+                   ((     ))
+               ===  \\_v_//  ===
+ Art by          ====)_^_(====
+Roland Waylor    ===/ O O \===
+                 = | /_ _\ | =
+                =   \/_ _\/   =
+                     \_ _/
+                     (o_o)
+                      VwV
+"""
+
+    print(logo)
+    print("Boas-vindas ao FIBULA RPG")
+    mostrar_logo()
